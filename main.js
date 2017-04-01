8929dcc3d162d6855cbd83ba3e96e762918eb1ad
musan63 <i151336@gm.ishikawa-nct.ac.jp>
2017-04-01 10:51:05 +0900

    current
    更新

--- main.js
+++ main.js
@@ -5,13 +5,6 @@ $(function() {
   const $hashtag  = $('#hashtag');
   const $accountId = $('#accountId');
   const $submit    = $('#submit');
-  /*
-  let $keyword_val = null;
-  let $hashtag_val = null;
-  let $accountId_val = null;
-  // jsで変数はvarでなくlet?
-  // さらに必要性低そう
-  */
 
 /*
   // フォームチェック関数
@@ -42,8 +35,6 @@ $(function() {
   // フォームが送信された時にアラートを表示する
   $('form').on('submit', (e) => {
     e.preventDefault();
-    // console.log( $keyword_val + "\n" + $hashtag_val + "\n" + $accountId_val );
-    // console.logに直接処理を書く
     console.log( "keyword : " + formval($keyword) + "\n" + "hashtag : " + formval($hashtag) + "\n" + "accountId : " + formval($accountId) );
   });
 });
